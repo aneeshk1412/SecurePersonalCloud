492b93a45b69007b765e0f664c01ea4006279adf
aneesh-kaitou1412 <a3grand@gmail.com>
2018-11-27 23:20:13 +0530

    adds a user app to server

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # rest framework import
+    'rest_framework',
+
+    # app imports
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
---
+++ Server/user/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class DirFile(models.Model):
+    # Basic File Details
+
+    name = models.CharField(max_length=5000)
+    file_type = models.CharField(max_length=20)
+    modified_time = models.DateTimeField(auto_now=True)
+    encryption_scheme = models.CharField(max_length=100)
+    md5code = models.TextField()
+    file_path = models.TextField()
+
+    # User Details , to be changed for file sharing
+
+    # File Contents , to be changed for block level encryption
+
+    file_contents = models.TextField()
+
+    def __str__(self):
+        name = str(self.name)
+        file_type = str(self.file_type)
+        encryption_scheme = str(self.encryption_scheme)
+        res = "Name : " + name + " Type : " + file_type + " Scheme : " + encryption_scheme
+        return res
